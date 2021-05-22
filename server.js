02e5c78e61b239d906e5aade23da2591dbfc1130
Jakob Cvetko <jakob.cvetko@gmail.com>
2021-05-22 17:17:13 +0200

    Update

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ io.on('connection', (socket) => {
 io.sockets.on('connection', function(socket) {
   // once a client has connected, we expect to get a ping from them saying what room they want to join
   socket.on('room', function(room) {
+      console.log("Client joined room", room)
       socket.join(room);
   });
 });
