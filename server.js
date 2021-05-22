c1fb0b0d5e19046a36b9f379d46070ab558bda1f
Jakob Cvetko <jakob.cvetko@gmail.com>
2021-05-22 16:59:41 +0200

    Setup websocket server

--- server.js
+++ server.js
@@ -4,11 +4,39 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
+const API_SECRET = process.env.API_SECRET || 'local';
 const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+var app = express();
+app.use(express.json());
+
+app.post('/channels/:id', function(request, response) {
+
+  // Security check
+  if(request.headers.authorization != API_SECRET) {
+    response.writeHead(401, {'Content-Type': 'text/json'});
+    response.end(null);
+    return;
+  }
+
+  // Emit to everyone who listens on this channel
+  io.to(request.params.id).emit('update', request.body)
+
+  // Response
+  response.writeHead(200, {'Content-Type': 'text/json'});
+  response.end();
+});
+
+// Server start
+const server = app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
+
+//
+// Socket.io logic
+//
 
 const io = socketIO(server);
 
@@ -17,4 +45,9 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+io.sockets.on('connection', function(socket) {
+  // once a client has connected, we expect to get a ping from them saying what room they want to join
+  socket.on('room', function(room) {
+      socket.join(room);
+  });
+});
