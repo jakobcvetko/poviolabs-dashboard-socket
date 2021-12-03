d792a513d3a11afff4b08cee663ff6f71c7ce14a
Jakob Cvetko <jakob.cvetko@gmail.com>
2021-12-03 16:48:39 +0100

    Increased limit for json size

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const INDEX = '/index.html';
 
 var app = express();
 app.use(cors())
-app.use(express.json());
+app.use(express.json({limit: '100mb'}));
 
 app.post('/channels/:id', function(request, response) {
 
